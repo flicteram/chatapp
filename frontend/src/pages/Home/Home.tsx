e47ec9a5817ebb5e9700493b62799a0d3db01098
Alexandru Flicter <v-aflicter@microsoft.com>
2023-07-13 16:22:12 +0300

    add storage request access

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,13 +2,48 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import styles from './Home.module.css'
 import { useNavigate } from 'react-router-dom'
 import Layout from "../../components/Layout/Layout";
+import { Drawer } from '@mui/material';
+import { useState, useEffect } from 'react'
 
 function Home() {
   const navigate = useNavigate()
 
+  const [isOpen, setIsOpen] = useState( false )
+
+  const toggleModal = ()=> setIsOpen( prev=>!prev )
+
+  const requestStorageAccess = () =>{
+    if( typeof document.requestStorageAccess === 'function' ){
+      document.requestStorageAccess().then(
+        () => {
+          toggleModal()
+        },
+        () => {
+          console.log( "access denied" );
+        },
+      );
+    }
+  }
+  useEffect( ()=>{
+    if( typeof document.hasStorageAccess === 'function' ){
+      document.hasStorageAccess().then( ( hasAccess )=>{
+        if( !hasAccess ){
+          toggleModal()
+        }
+      })
+    }
+  }, [])
   return (
     <Layout>
       <div className={styles.homeContainer}>
+        <Drawer
+          open={isOpen}
+          anchor='bottom'
+        >
+          <h1>Accept cookies?</h1>
+          <button onClick={requestStorageAccess}>Yes</button>
+          <button >No</button>
+        </Drawer>
         <h1>Connect with people around the world!</h1>
         <WhatsAppIcon
           style={{
