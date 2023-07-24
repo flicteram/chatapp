001594b261f17d5b6a5c9362fbe7bdba3d74b7d5
flicteram <85151322+flicteram@users.noreply.github.com>
2023-07-24 15:26:12 +0300

    Delete frontend/src/components directory

--- frontend/src/components/HandleAuthRoute/HandleAuthRoute.tsx
+++
@@ -1,9 +0,0 @@
-import useUserSelector from "Components/User/useUserSelector"
-import { Navigate, Outlet } from 'react-router-dom'
-export default function HandleAuthRoute() {
-  const currentUser = useUserSelector()
-  if ( currentUser.accessToken ) {
-    return <Navigate to="/chat" />
-  }
-  return <Outlet />
-}
